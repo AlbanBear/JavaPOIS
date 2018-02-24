8c92278c3f3b83e02c8c8a24d18fe47c74bc6ecc
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-02-24 17:54:29 +0300

    Update Lab1.java

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,4 +1,4 @@
-public class Lab1{
+public class Lab1{ 
 	public static void main(String[] args){
 		System.out.println("Name and surname: \t" + "Ivan Panamarou");
 		System.out.println("Number of group: \t" + "POIS1709v1");
