ae5c9fc7f30d773f337b01e9b797601108973390
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-02-24 17:50:22 +0300

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,8 @@
+public class Lab1{
+	public static void main(String[] args){
+		System.out.println("Name and surname: \t" + "Ivan Panamarou");
+		System.out.println("Number of group: \t" + "POIS1709v1");
+		System.out.println("Current occupation: \t" + "Temporary unemployed");
+		System.out.println("Hobbies: \t\t" + "Music, Internet, travel, ski rides, quizzes");
+	}
+}
