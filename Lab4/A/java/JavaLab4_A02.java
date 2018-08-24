b62fe81d751b5a89eea1a6f1579458481309f4a5
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-25 01:02:55 +0300

    Update JavaLab4_A02.java

--- Lab4/A/java/JavaLab4_A02.java
+++ Lab4/A/java/JavaLab4_A02.java
@@ -6,16 +6,16 @@ package javalab4_A;
  */
 public class JavaLab4_A02 {
     
-//    public static void main(String[] args) {
-//       System.out.println ("Task 2: Are there coincident numbers among A, B and C?");
-//       double a = 3.62;
-//       double b = 4.70;
-//       double c = -5.20;
-//       boolean result = Logic.checkIfCoincident(a, b, c);
-//       System.out.println ("A="+a);
-//       System.out.println ("B="+b);
-//       System.out.println ("C="+c);
-//       System.out.println ("Answer: "+result);
-//    }
+   public static void main(String[] args) {
+      System.out.println ("Task 2: Are there coincident numbers among A, B and C?");
+      double a = 3.62;
+      double b = 4.70;
+      double c = -5.20;
+      boolean result = Logic.checkIfCoincident(a, b, c);
+      System.out.println ("A="+a);
+      System.out.println ("B="+b);
+      System.out.println ("C="+c);
+      System.out.println ("Answer: "+result);
+   }
  
 }
