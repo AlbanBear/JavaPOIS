f16c99a9abce6ab1a2341a1681dfa260b2d367d5
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-25 01:07:53 +0300

    Update JavaLab4_A13.java

--- Lab4/A/java/JavaLab4_A13.java
+++ Lab4/A/java/JavaLab4_A13.java
@@ -5,11 +5,11 @@ package javalab4_A;
  * @author Ivan Panamarou
  */
 public class JavaLab4_A13 {
-//       public static void main(String[] args) {
-//       System.out.println ("Task 13: Is number N parindrome (reads the same backward as forward)?");
-//       int n = 2442;
-//       boolean result = Logic.checkPalindrome(n);
-//       System.out.println ("N="+n);
-//       System.out.println ("Answer: "+result);
-//    }
+      public static void main(String[] args) {
+      System.out.println ("Task 13: Is number N parindrome (reads the same backward as forward)?");
+      int n = 2442;
+      boolean result = Logic.checkPalindrome(n);
+      System.out.println ("N="+n);
+      System.out.println ("Answer: "+result);
+   }
 }
