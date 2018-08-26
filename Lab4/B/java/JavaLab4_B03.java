b4cfee4a82b1706c06bc35455a8967a2306bca5c
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-26 23:25:09 +0300

    Update JavaLab4_B03.java

--- Lab4/B/java/JavaLab4_B03.java
+++ Lab4/B/java/JavaLab4_B03.java
@@ -6,18 +6,18 @@ package javalab4_B;
  * @author Ivan Panamarou
  */
 public class JavaLab4_B03 {
-//    public static void main(String[] args) {
-//        int gr = 34_675_120;
-//        System.out.println("Task 3: Weight of the dinosaur is given in grams."
-//                + "\nCalculate the weight in kilograms, centners, tons.");
-//        double kg = B03_WeightCalculator.calculateKgs(gr);
-//        double c = B03_WeightCalculator.calculateCs(gr);
-//        double t = B03_WeightCalculator.calculateTs(gr);
-//        System.out.println("Weight of the dinosaur: ");
-//        System.out.println(gr+" Grams");
-//        System.out.println("or "+kg+" Kilograms");
-//        System.out.println("or "+c+" Centners");
-//        System.out.println("or "+t+" Tonnes");
-//        
-//    }
+   public static void main(String[] args) {
+       int gr = 34_675_120;
+       System.out.println("Task 3: Weight of the dinosaur is given in grams."
+               + "\nCalculate the weight in kilograms, centners, tons.");
+       double kg = B03_WeightCalculator.calculateKgs(gr);
+       double c = B03_WeightCalculator.calculateCs(gr);
+       double t = B03_WeightCalculator.calculateTs(gr);
+       System.out.println("Weight of the dinosaur: ");
+       System.out.println(gr+" Grams");
+       System.out.println("or "+kg+" Kilograms");
+       System.out.println("or "+c+" Centners");
+       System.out.println("or "+t+" Tonnes");
+       
+   }
 }
