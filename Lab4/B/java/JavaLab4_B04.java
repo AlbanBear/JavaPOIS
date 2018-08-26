c7912ae0032e5802e9b6b9cc8d60147329fbd5c7
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-26 23:25:50 +0300

    Update JavaLab4_B04.java

--- Lab4/B/java/JavaLab4_B04.java
+++ Lab4/B/java/JavaLab4_B04.java
@@ -6,15 +6,15 @@ import java.util.Date;
  * @author Ivan Panamarou
  */
 public class JavaLab4_B04 {
-//    public static void main(String[] args) {
-//        System.out.println("Task 4: Find out how many seconds, minutes and hours ago."
-//                + "\nthe current day started?.");
-//        Date curDate = new Date(); 
-//        System.out.println("Today is : " + curDate); 
-//        System.out.println("The day began " + B04_TimeCalculator.todayInHrs() + " hours");  
-//        System.out.println("or " + B04_TimeCalculator.todayInMin() + " minutes");  
-//        System.out.println("or " + B04_TimeCalculator.todayInSec() + " seconds ago.");
-//    }
-//        
+   public static void main(String[] args) {
+       System.out.println("Task 4: Find out how many seconds, minutes and hours ago."
+               + "\nthe current day started?.");
+       Date curDate = new Date(); 
+       System.out.println("Today is : " + curDate); 
+       System.out.println("The day began " + B04_TimeCalculator.todayInHrs() + " hours");  
+       System.out.println("or " + B04_TimeCalculator.todayInMin() + " minutes");  
+       System.out.println("or " + B04_TimeCalculator.todayInSec() + " seconds ago.");
+   }
+       
  }
 
