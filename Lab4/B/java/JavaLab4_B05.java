7c62a1b7f618526a5b9cc1181e2e9bb9003c5726
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-26 23:25:33 +0300

    Update JavaLab4_B05.java

--- Lab4/B/java/JavaLab4_B05.java
+++ Lab4/B/java/JavaLab4_B05.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
  * @author Ivan Panamarou
  */
 public class JavaLab4_B05 {
-//    public static void main(String[] args) {
-//        System.out.println("Task 5: Find the product of digits"
-//        + "\nof the decimal number.");
-//        int n;
-//        System.out.println("\nPlease enter number:");
-//        Scanner scanner = new Scanner(System.in);
-//        n = scanner.nextInt();
-//        int pr = B05_DigitsProduct.digitsProduct(n);
-//        System.out.println("The number is " + n);
-//        System.out.println("The product is " + pr);
-//        
-//    }
+   public static void main(String[] args) {
+       System.out.println("Task 5: Find the product of digits"
+       + "\nof the decimal number.");
+       int n;
+       System.out.println("\nPlease enter number:");
+       Scanner scanner = new Scanner(System.in);
+       n = scanner.nextInt();
+       int pr = B05_DigitsProduct.digitsProduct(n);
+       System.out.println("The number is " + n);
+       System.out.println("The product is " + pr);
+       
+   }
 }
