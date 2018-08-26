761ff55ab354054d583835050901e75d648b711b
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-26 23:24:45 +0300

    Update JavaLab4_B02.java

--- Lab4/B/java/JavaLab4_B02.java
+++ Lab4/B/java/JavaLab4_B02.java
@@ -6,18 +6,18 @@ package javalab4_B;
  * @author Ivan Panamarou
  */
 public class JavaLab4_B02 {
-//    public static void main(String[] args) {
-//        int cm = 2_078_916_127;
-//        System.out.println("Task 2: Distance from one town to another is given in centimetres."
-//                + "\nCalculate the distance in metres and kilometres.");
-//        double m = B02_DistanceCalculator.calculateMetres(cm);
-//        double km = B02_DistanceCalculator.calculateKilometres(cm);
-//
-//        
-//        System.out.println("Distance: ");
-//        System.out.println(cm+" Centimetres");
-//        System.out.println("or "+m+" Metres");
-//        System.out.println("or "+km+" Kilometres");
-//
-//    }
+   public static void main(String[] args) {
+       int cm = 2_078_916_127;
+       System.out.println("Task 2: Distance from one town to another is given in centimetres."
+               + "\nCalculate the distance in metres and kilometres.");
+       double m = B02_DistanceCalculator.calculateMetres(cm);
+       double km = B02_DistanceCalculator.calculateKilometres(cm);
+
+       
+       System.out.println("Distance: ");
+       System.out.println(cm+" Centimetres");
+       System.out.println("or "+m+" Metres");
+       System.out.println("or "+km+" Kilometres");
+
+   }
 }
