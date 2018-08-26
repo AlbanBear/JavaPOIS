49c8b85e0b0ddbae8d75f604b28da983bbe93740
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-26 23:29:23 +0300

    Update JavaLab4_B07.java

--- Lab4/B/java/JavaLab4_B07.java
+++ Lab4/B/java/JavaLab4_B07.java
@@ -1,19 +1,19 @@
 
 
-package javalab4_b;
+package javalab4_B;
 import java.util.Scanner;
 /**
  * @author Ivan Panamarou
  */
 public class JavaLab4_B07 {
-//    public static void main(String[] args) {
-//        System.out.println("Task 7: Reverse the multiplace number");
-//        int n;
-//        System.out.println("Please enter number");
-//        Scanner scanner = new Scanner (System.in);
-//        n = scanner.nextInt();
-//        int rev = B07_Reverse.reverse(n);
-//        System.out.println("Initial number: " + n);
-//        System.out.println("Reversed number: " + rev);
-//    }
+   public static void main(String[] args) {
+       System.out.println("Task 7: Reverse the multiplace number");
+       int n;
+       System.out.println("Please enter number");
+       Scanner scanner = new Scanner (System.in);
+       n = scanner.nextInt();
+       int rev = B07_Reverse.reverse(n);
+       System.out.println("Initial number: " + n);
+       System.out.println("Reversed number: " + rev);
+   }
 }
