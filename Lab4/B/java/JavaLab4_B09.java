f9fc7b33573c3b2aada8e0fe7289bc0a5d1c9da7
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-26 23:30:11 +0300

    Update JavaLab4_B09.java

--- Lab4/B/java/JavaLab4_B09.java
+++ Lab4/B/java/JavaLab4_B09.java
@@ -1,20 +1,20 @@
 
 
-package javalab4_b;
+package javalab4_B;
 
 /**
  * @author Ivan Panamarou
  */
 public class JavaLab4_B09 {
-//    public static void main(String[] args) {
-//        System.out.println("Task 9: How many squares with side A will "
-//        + "\ngo into the rectangle with sides B and C? How much area is left?");
-//        double a = 3.25;
-//        double b = 18.57;
-//        double c = 28.93;
-//        int sqCounter = B09_SquareCounter.countSquares(a, b, c);
-//        double frArea = B09_SquareCounter.freeArea(a, b, c);
-//        System.out.println(sqCounter + " squares will go into rectangle");
-//        System.out.println("Blank area: " + frArea);
-//    }
+   public static void main(String[] args) {
+       System.out.println("Task 9: How many squares with side A will "
+       + "\ngo into the rectangle with sides B and C? How much area is left?");
+       double a = 3.25;
+       double b = 18.57;
+       double c = 28.93;
+       int sqCounter = B09_SquareCounter.countSquares(a, b, c);
+       double frArea = B09_SquareCounter.freeArea(a, b, c);
+       System.out.println(sqCounter + " squares will go into rectangle");
+       System.out.println("Blank area: " + frArea);
+   }
 }
