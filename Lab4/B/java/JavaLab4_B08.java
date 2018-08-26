ed2587abce0169b3df60ec1ec7fda06353191600
Ivan Panamaroū <32228599+AlbanBear@users.noreply.github.com>
2018-08-26 23:23:42 +0300

    Add files via upload

---
+++ Lab4/B/java/JavaLab4_B08.java
@@ -0,0 +1,53 @@
+
+
+package javalab4_b;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+/**
+ * @author Ivan Panamarou
+ */
+public class JavaLab4_B08 {
+//    public static void main(String[] args) throws Exception{
+//        System.out.println("Task 8: Find out the weekday of the input date "
+//        + "\nPlease enter date in dd mm yyyy (through spaces!"
+//                + "\nno dots, slashes, etc.) format:");
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        String tempDate = reader.readLine();
+//        SimpleDateFormat simpleFormat = new SimpleDateFormat("dd MM yyyy");
+//        SimpleDateFormat simpleFormatS = new SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH);
+//        Date date = simpleFormat.parse(tempDate);
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTime(date);
+//        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+//        String dow;
+//        switch(dayOfWeek){
+//            case 1:
+//                dow="Sunday";
+//                break;
+//            case 2:
+//                dow="Monday";
+//                break;
+//            case 3:
+//                dow="Tuesday";
+//                break;                
+//            case 4:
+//                dow="Wednesday";
+//                break;
+//            case 5:
+//                dow="Thursday";
+//                break; 
+//            case 6:
+//                dow="Friday";
+//                break;
+//            default:
+//                dow="Saturday";
+//                break;
+//                
+//        }
+//        System.out.println("Date: "+simpleFormatS.format(date));
+//        System.out.println("Day of week: "+dow);
+//        }
+}
+
